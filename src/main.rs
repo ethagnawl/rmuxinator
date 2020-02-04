dd7c09390b9e73dac067339172600c30252fd511
Peter Doherty <pdoherty@protonmail.com>
2020-02-03 21:58:33 -0500

    better error messages in main

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let cli_args = CliArgs::new(&args).unwrap_or_else(|error| {
-        eprintln!("Problem parsing arguments: {}", error);
+        eprintln!("Problem parsing CLI arguments: {}", error);
         process::exit(1);
     });
 
     println!("main.rs {:?}", cli_args);
 
     let config = Config::new(cli_args).unwrap_or_else(|error| {
-        eprintln!("Problem parsing arguments: {}", error);
+        eprintln!("Problem parsing config file: {}", error);
         process::exit(1);
     });
 
